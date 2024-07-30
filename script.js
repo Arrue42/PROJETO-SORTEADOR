a2857d7b1ca67b65198697051926d238ff6df285
Arrue42 <paoloarrue27@gmail.com>
2024-07-30 18:46:51 -0300

    adicionados o IF e ELSE no JS

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ function generateNumber(){
 const min = Math.ceil(document.querySelector(".input-min").value)
 const max = Math.floor(document.querySelector(".input-max").value)
 
-const result = Math.floor(Math.random() * (max - min + 1)) + min;
-   
- alert(result)
+if( min >= max){
+    alert ("O valor mnimo não poder ser maior que o valor maximo!!")
 }
+
+else{
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+   
+    alert(result)
+   }
+
+}
+
